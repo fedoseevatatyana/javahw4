784e77aa2ca7aac0d28edc29016608eaba487683
fedoseevatatyana <74595766+fedoseevatatyana@users.noreply.github.com>
2020-12-14 11:05:04 +0300

    Update BonusMilesService.java

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,9 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
         int miles = cost / 20;
-        if (cost < 20) {
-            miles = 0;
-        }
         return miles;
     }
 }
